0b3abf5c101f036ef6305e95e77942a36586bd2e
Gavin <it-ldh@top-ele.com.cn>
2015-09-12 16:01:08 +0800

    Initial Commit

---
+++ ColorGradualProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorGradualProgress
+//
+//  Created by dhuil on 15/9/12.
+//  Copyright (c) 2015年 Gavin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColorGradualProgress/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ColorGradualProgress
+//
+//  Created by dhuil on 15/9/12.
+//  Copyright (c) 2015年 Gavin Li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
